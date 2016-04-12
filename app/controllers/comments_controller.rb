b43daca212f555ab3289701fcfe6b431970ebfd1
Arvin Hadisurya <akhadisurya@gmail.com>
2016-04-12 15:20:41 -0700

    some authentications work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, except: [:index, :show]
+  before_action :only_my_comments, only: [:edit, :update]
 
     def index
     @comments = Comment.all
@@ -10,7 +13,7 @@ class CommentsController < ApplicationController
 
     def create
     @comment = Comment.new(params.require(:comment).permit(:content, :user_id, :match_id))
-
+    @comment.user = current_user
         if @comment.save
           redirect_to match_path(@comment.match)
        else
@@ -38,5 +41,21 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to match_path(@comment.match)
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :match_id)
+    end
+
+    def only_my_comments
+      redirect_to match_path(@comment.match), notice: "you can't edit someone elses tweets" if (current_user != @comment.user)
+    end
+
 
 end
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,5 @@
 class MatchesController < ApplicationController
-  # before_action :set_match, only: [:show, :edit, :update, :destroy]
-  # before_action :authorize, except: [:index, :show]
+
 
     def index
     @matches = Match.all
