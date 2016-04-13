9093e6b85bff733e607f19cc54ab18f8296fd7d1
Arvin Hadisurya <akhadisurya@gmail.com>
2016-04-13 10:52:09 -0700

    picts for matches

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412233052) do
+ActiveRecord::Schema.define(version: 20160413163500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160412233052) do
     t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "matches_users", id: false, force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Match.destroy_all
+
+
+
+  Match.create([
+        {first_name: "Emma", last_name: "Watson", gender: "Female", age: 22, image: "http://images-cdn.moviepilot.com/image/upload/v1426602397/bb1f41c779b62689a8bc5dc7824db057_large-it-felt-like-incest-emma-watson-expresses-her-views-on-kissing-rupert-grint-jpeg-236998.jpg"},
+        {first_name: "Lana", last_name: "Del Rey", gender: "Female", age: 26, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb-VS5GJK3SfXvtr9f-ApYKG15uCubCn8CZmC5VgxIFHXXG-Qi"},
+        {first_name: "Olivia", last_name: "Munn", gender: "Female", age: 36, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZGFeLWSBrVM2KQqHCy9bp-HdEIdqm8J8ftxAoWbHqygDOCk0b"},
+        {first_name: "Emily", last_name: "Ratajkowski", gender: "Female", age: 22, image:"http://img2-3.timeinc.net/people/i/2016/news/160125/emily-ratajkowski-435.jpg"},
+
+    ])
+
