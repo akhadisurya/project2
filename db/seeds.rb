cbea1be97420312cb1cc70983866a7e13b31a4a8
Arvin Hadisurya <akhadisurya@gmail.com>
2016-04-13 17:30:31 -0700

    added more matches

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@ Match.destroy_all
         {first_name: "Lana", last_name: "Del Rey", gender: "Female", age: 26, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb-VS5GJK3SfXvtr9f-ApYKG15uCubCn8CZmC5VgxIFHXXG-Qi"},
         {first_name: "Olivia", last_name: "Munn", gender: "Female", age: 36, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZGFeLWSBrVM2KQqHCy9bp-HdEIdqm8J8ftxAoWbHqygDOCk0b"},
         {first_name: "Emily", last_name: "Ratajkowski", gender: "Female", age: 22, image:"http://img2-3.timeinc.net/people/i/2016/news/160125/emily-ratajkowski-435.jpg"},
-
+        {first_name: "Taylor", last_name: "Swift", gender: "Female", age: 25, image:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTu5sS9_kpPTltx3JjWcZgSME4pWkCjhsc7aky1kS-Cy7z3GkgsSw"},
+        {first_name: "Leonardo", last_name: "DiCaprio", gender: "Male", age: 42, image:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS7V4h1RYmM34O30hTubT4gRS0AA62glrMLJbvIcM5nDopm3hZI"},
+        {first_name: "Zac", last_name: "Efron", gender: "Male", age: 27, image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfduhpyeqNYpggOK8wolGrdoiSzmUQJWXE6G6ewJj5etvMtNOTYg"},
     ])
 
